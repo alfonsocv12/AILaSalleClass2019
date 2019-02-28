37aa71b7c7ec62bf46acbb7a9de2eb48e89cab4d
alfonsocv12 <alfonsocvu@gmail.com>
2019-02-28 09:41:28 -0700

    Se agrego plotly

--- practica2/rentalListingInquiries.py
+++ practica2/rentalListingInquiries.py
@@ -1,4 +1,4 @@
-import numpy as np, pandas as pd
+import numpy as np, pandas as pd, plotly, plotly.plotly as py, plotly.graph_objs as go, matplotlib.pyplot as plt
 
 from sklearn.model_selection import train_test_split
 from sklearn.tree import DecisionTreeClassifier
@@ -7,7 +7,7 @@ rental_listing = pd.read_json('train.json')
 
 rental_listing = rental_listing.drop(['features', 'photos'], axis=1)
 
-print(rental_listing.head(10))
+print(rental_listing.head(20))
 
 print(rental_listing.describe())
 
@@ -15,13 +15,9 @@ print(rental_listing.dtypes)
 
 print(rental_listing.apply(lambda x: x.isnull().any()))
 
-#print(pd.DataFrame({'percent_missing': rental_listing.isnull().sum() * 100 / len(rental_listing)}))
+print(pd.DataFrame({'percent_missing': rental_listing.isnull().sum() * 100 / len(rental_listing)}))
 
-#print(pd.DataFrame({'percent_unique': rental_listing.apply(lambda x: x.unique().size/x.size*100)}))
-
-# target_class = rental_listing.interest_level.value_counts().values.tolist()
-#
-# print(target_class)
+print(pd.DataFrame({'percent_unique': rental_listing.apply(lambda x: x.unique().size/x.size*100)}))
 
 rental_listing['building_id'] = pd.Categorical(rental_listing.building_id)
 rental_listing['building_id'] = pd.get_dummies(rental_listing['building_id'])
@@ -42,14 +38,34 @@ rental_listing['street_address'] = pd.get_dummies(rental_listing['street_address
 rental_listing['interest_level'] = pd.Categorical(rental_listing.interest_level)
 rental_listing['interest_level'] = pd.get_dummies(rental_listing['interest_level'])
 
+plotly.tools.set_credentials_file(username='alfonsocv18', api_key='IukOlHfoQOc9CejJEThc')
+
+def plotBox(plot_data, msg):
+    print(plot_data.index.values)
+    print(plot_data.columns.values)
+    # trace0 = go.Box(
+    #     y=y0
+    # )
+    # trace1 = go.Box(
+    #     y=y1
+    # )
+    # data = [trace0, trace1]
+    #
+    # py.iplot(data)
+
+
+
 data_features = rental_listing.values
 data_labels = rental_listing['interest_level'].values
+bathrooms_data = pd.crosstab(rental_listing.interest_level, rental_listing.bathrooms)
+# print(bathrooms_data)
+plotBox(bathrooms_data, 'Interes en base a la cantidad de banos')
 
 X_train, X_test, Y_train, Y_test = train_test_split(data_features, data_labels, test_size=0.3)
 
-model = DecisionTreeClassifier()
-model.fit(X_train, Y_train)
-
-score = model.score(X_test, Y_test)
-
-print(score)
+# model = DecisionTreeClassifier()
+# model.fit(X_train, Y_train)
+#
+# score = model.score(X_test, Y_test)
+#
+# print(score)
